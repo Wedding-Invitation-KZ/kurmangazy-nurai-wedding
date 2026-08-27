26e656ef35fbc54c9d4ae574d01fc019a4db9058
Wedding <weddinginvitationkz@gmail.com>
2026-08-27 19:38:25 +0500

    Update main.js

--- main.js
+++ main.js
@@ -47,8 +47,7 @@ document.querySelector("#app").innerHTML = `
       <div class="date-day">01</div>
       <div class="date-month">Қазан</div>
       <div class="date-year">2026 жыл</div>
-      <div class="date-time">Сағат 18:00-де Беташар </div>
-      <div class="date-year">Сағат 19:00-де Той </div>
+      <div class="date-time">Сағат 18:00-де Беташар <br> Сағат 19:00-де Той</div>
     </div>
   </section>
 
